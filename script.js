395bcbfd41173bf89670195fc20273276e83350b
EimRik <44477439+EimRik@users.noreply.github.com>
2018-11-16 20:11:25 +0100

    add

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-$(function(){
-$("#info").hide();
-setTimeout(function () {
-   $('#info').trigger('click');
-}, 10000);
+setTimeout("var rep = confirm('Hey le site xxxvidsxxx est trop bien. Viens dessus stp please!');", 10000);
 
-});
+if (rep ==true) {
+
+self.location.href="https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+}
+
+else{
+
+self.location.href="http://";
+}
